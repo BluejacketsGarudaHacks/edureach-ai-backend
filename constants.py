eada5f7abffb1d6081dd03cfab8d2d6782cc315f
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-07-24 15:28:44 +0700

    feat: constants for application

---
+++ constants.py
@@ -0,0 +1,16 @@
+LANGUAGES = {
+    "bali": "ban",
+    "jawa": "jw",
+    "sunda": "su",
+    "batak karo": "btx",   
+    "batak simalungun": "bts",
+    "batak toba": "bbc",
+    "minang": "min",
+}
+
+BATCH_SIZE=3
+
+SOURCE_LANGUAGES = {
+    'english': 'en',
+    'indonesia': 'id'
+}
